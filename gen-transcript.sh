65e2ec6bd1816d9e694e0bbf442b628f7854702a
Yifan <guyifan1121@gmail.com>
2023-06-21 16:21:43 -0700

    Add optional model choice

--- gen-transcript.sh
+++ gen-transcript.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Usage: gen-transcript.sh file [language]
+# Usage: gen-transcript.sh file [language] [model]
 
 PATH_TO_WHISPER="${PATH_TO_WHISPER:-${HOME}/github.com/ggerganov/whisper.cpp}"
 FILE=${1}
 LANG=${2:-chinese}
+MODEL=${3:-small}
+FILEBASE=$(basename ${FILE%.MP4})
 
-ffmpeg -i ${FILE}  -ar 16000 -ac 1 -c:a pcm_s16le ${FILE%.MP4}.wav -y;
-${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-small.bin -l ${LANG} -osrt -f ${FILE%.MP4}.wav -of ${FILE%.MP4}
+ffmpeg -i ${FILE} -ar 16000 -ac 1 -c:a pcm_s16le ${FILEBASE}.wav -y;
+${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-${MODEL}.bin -l ${LANG} -osrt -f ${FILEBASE}.wav -of ${FILEBASE}
 
-rm -fr ${FILE%.MP4}.wav
+rm -fr ${FILEBASE}.wav
