38da4e7df7a2aad64967382066515a8cb56405de
Yifan <guyifan1121@gmail.com>
2023-06-21 16:15:06 -0700

    Rename gen-transcript.sh to gen-transcript-batch.sh
    Modified gen-transcript.sh to transcript only one file.

---
+++ gen-transcript-batch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: gen-transcript.sh [language]
+
+PATH_TO_WHISPER="${PATH_TO_WHISPER:-${HOME}/github.com/ggerganov/whisper.cpp}"
+LANG=${1:-chinese}
+
+for f in `ls *.MP4`;do
+  ffmpeg -i $f  -ar 16000 -ac 1 -c:a pcm_s16le ${f%.MP4}.wav -y;
+done
+
+mkdir transcript
+mv *.wav transcript
+
+cd transcript
+for f in `ls *.wav`;do
+  ${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-small.bin -l ${LANG} -osrt -f $f -of ${f%.wav}
+done
+
+rm -fr *.wav
--- gen-transcript.sh
+++ gen-transcript.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Usage: gen-transcript.sh [language]
+# Usage: gen-transcript.sh file [language]
 
 PATH_TO_WHISPER="${PATH_TO_WHISPER:-${HOME}/github.com/ggerganov/whisper.cpp}"
-LANG=${1:-chinese}
+FILE=${1}
+LANG=${2:-chinese}
 
-for f in `ls *.MP4`;do
-  ffmpeg -i $f  -ar 16000 -ac 1 -c:a pcm_s16le ${f%.MP4}.wav -y;
-done
+ffmpeg -i ${FILE}  -ar 16000 -ac 1 -c:a pcm_s16le ${FILE%.MP4}.wav -y;
+${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-small.bin -l ${LANG} -osrt -f ${FILE%.MP4}.wav -of ${FILE%.MP4}
 
-mkdir transcript
-mv *.wav transcript
-
-cd transcript
-for f in `ls *.wav`;do
-  ${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-small.bin -l ${LANG} -osrt -f $f -of ${f%.wav}
-done
-
-rm -fr *.wav
+rm -fr ${FILE%.MP4}.wav
