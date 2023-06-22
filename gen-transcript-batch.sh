b85c1c49ab07ea8b456ec730fec627171205de8a
Yifan <guyifan1121@gmail.com>
2023-06-21 17:36:41 -0700

    Add model option to gen-transcript-batch.sh

--- gen-transcript-batch.sh
+++ gen-transcript-batch.sh
@@ -4,6 +4,7 @@
 
 PATH_TO_WHISPER="${PATH_TO_WHISPER:-${HOME}/github.com/ggerganov/whisper.cpp}"
 LANG=${1:-chinese}
+MODEL=${2:-small}
 
 for f in `ls *.MP4`;do
   ffmpeg -i $f  -ar 16000 -ac 1 -c:a pcm_s16le ${f%.MP4}.wav -y;
@@ -14,7 +15,7 @@ mv *.wav transcript
 
 cd transcript
 for f in `ls *.wav`;do
-  ${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-small.bin -l ${LANG} -osrt -f $f -of ${f%.wav}
+  ${PATH_TO_WHISPER}/main -m ${PATH_TO_WHISPER}/models/ggml-${MODEL}.bin -l ${LANG} -osrt -f $f -of ${f%.wav}
 done
 
 rm -fr *.wav
