1675fbfe209bb99d290c2b4c28099f2e8e9dbf24
Yifan <guyifan1121@gmail.com>
2024-07-11 01:39:18 -0700

    reorg-script.sh: Add support for lark max folder.

--- reorg-script.sh
+++ reorg-script.sh
@@ -3,6 +3,7 @@
 FX3_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/fx3"
 AX53_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/ax53"
 TENTACLE_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/tentacle track e"
+LARK_MAX_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/lark max"
 
 echo "Sync ${TENTACLE_DIR}"
 
@@ -22,4 +23,20 @@ for dir in $(ls "${AX53_DIR}"); do
         echo "Creating symlink for ${dir}"
         ln -s "${AX53_DIR}/${dir}" "${FX3_DIR}/${dir}/ax53"
     fi
+done
+
+echo "Sync ${LARK_MAX_DIR}"
+
+for mic in "MIC 1" "MIC 2"; do
+    for lark_dir in $(ls "${LARK_MAX_DIR}/${mic}"); do
+        dir_year=$(echo ${lark_dir} | awk -F "-" {'print $2'})
+        dir_month=$(echo ${lark_dir} | awk -F "-" {'print $3'})
+        dir_day=$(echo ${lark_dir} | awk -F "-" {'print $4'})
+        dir="${dir_year}${dir_month}${dir_day}"
+        cd "${FX3_DIR}"
+        if [ -d "${dir}" ] && [ 'template-dir' != "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/lark max ${mic}" ]; then
+            echo "Creating symlink for ${dir}/lark max/${mic}"
+            ln -s "${LARK_MAX_DIR}/${mic}/${lark_dir}" "${FX3_DIR}/${dir}/lark max ${mic}"
+        fi
+    done
 done
