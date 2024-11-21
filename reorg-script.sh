cdbd0151aa058de78ba635649a327e21c18c5a4d
Yifan <guyifan1121@gmail.com>
2024-11-21 05:28:30 -0800

    reorg-script.sh: Update folder name, remove template-dir.

--- reorg-script.sh
+++ reorg-script.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 #
-FX3_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/fx3"
-AX53_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/ax53"
-TENTACLE_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/tentacle track e"
-LARK_MAX_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2023/Raw Videos/lark max"
+FX3_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2024-Oct/Raw Videos/fx3"
+AX53_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2024-Oct/Raw Videos/ax53"
+TENTACLE_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2024-Oct/Raw Videos/tentacle track e"
+LARK_MAX_DIR="/Volumes/Archive-ZFS-8-Bay/Archive-2024-Oct/Raw Videos/lark max"
 
 echo "Sync ${TENTACLE_DIR}"
 
 for dir in $(ls "${TENTACLE_DIR}"); do
     cd "${FX3_DIR}"
-    if [ -d "${dir}" ] && [ 'template-dir' != "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/tentacle sync" ]; then
+    if [ -d "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/tentacle sync" ]; then
         echo "Creating symlink for ${dir}"
         ln -s "${TENTACLE_DIR}/${dir}" "${FX3_DIR}/${dir}/tentacle sync"
     fi
@@ -19,7 +19,7 @@ echo "Sync ${AX53_DIR}"
 
 for dir in $(ls "${AX53_DIR}"); do
     cd "${FX3_DIR}"
-    if [ -d "${dir}" ] && [ 'template-dir' != "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/ax53" ]; then
+    if [ -d "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/ax53" ]; then
         echo "Creating symlink for ${dir}"
         ln -s "${AX53_DIR}/${dir}" "${FX3_DIR}/${dir}/ax53"
     fi
@@ -34,7 +34,7 @@ for mic in "MIC 1" "MIC 2"; do
         dir_day=$(echo ${lark_dir} | awk -F "-" {'print $4'})
         dir="${dir_year}${dir_month}${dir_day}"
         cd "${FX3_DIR}"
-        if [ -d "${dir}" ] && [ 'template-dir' != "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/lark max ${mic}" ]; then
+        if [ -d "${dir}" ] && [ ! -d "${FX3_DIR}/${dir}/lark max ${mic}" ]; then
             echo "Creating symlink for ${dir}/lark max/${mic}"
             ln -s "${LARK_MAX_DIR}/${mic}/${lark_dir}" "${FX3_DIR}/${dir}/lark max ${mic}"
         fi
